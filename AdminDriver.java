dee2d8df4063a3ce1d48cac60b4ef7ca6a3637a9
SifaJ. <139639619+slf4A@users.noreply.github.com>
2024-12-01 09:19:15 +0700

    Update AdminDriver.java

--- AdminDriver.java
+++ AdminDriver.java
@@ -8,34 +8,34 @@ public class AdminDriver extends Driver {
   private ListBarang istBarang;
   private ArrayList<Transaksi> istTransaksi;
 
-  // Constructor
+ 
   public AdminDriver(Admin akun, ListBarang istBarang) {
       this.akun = akun;
       this.istBarang = istBarang;
-      this.istTransaksi = new ArrayList<>(); // Initialize the transaction list
+      this.istTransaksi = new ArrayList<>(); 
   }
 
-  // Getter for akun
+
   public Admin getAkun() {
       return akun;
   }
 
-  // Setter for akun
+  
   public void setAkun(Admin akun) {
       this.akun = akun;
   }
 
-  // Getter for istBarang
+  
   public ListBarang getIstBarang() {
       return istBarang;
   }
 
-  // Setter for istBarang
+  
   public void setIstBarang(ListBarang istBarang) {
       this.istBarang = istBarang;
   }
 
-  // Getter for istTransaksi
+ 
   public ArrayList<Transaksi> getIstTransaksi() {
       return istTransaksi;
   }
