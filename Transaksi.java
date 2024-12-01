09fdcd81ca2d4860d1de575f27c481c233eba8a0
DavinaAura <163745582+DavinaAura@users.noreply.github.com>
2024-12-01 12:04:58 +0700

    Transaksi.java

--- Transaksi.java
+++ Transaksi.java
@@ -1,39 +1,43 @@
-import java.util.ArrayList;
+class Transaksi {
+    Customer akun;
+    ArrayList<Barang> barang;
 
-public class Transaksi {
-    private String idTransaksi;
-    private Customer customer;
-    private ArrayList<ItemKeranjang> items;
-    private double totalHarga;
-    private String metodePembayaran;
-
-    public Transaksi(String idTransaksi, Customer customer, ArrayList<ItemKeranjang> items, double totalHarga, String metodePembayaran) {
-        this.idTransaksi = idTransaksi;
-        this.customer = customer;
-        this.items = items;
-        this.totalHarga = totalHarga;
-        this.metodePembayaran = metodePembayaran;
+    public Transaksi(Customer akun, ArrayList<Barang> barang) {
+        this.akun = akun;
+        this.barang = barang;
     }
+}
+class Invoice {
+    Transaksi transaksi;
+    Pembayaran pembayaran;
 
-    public String getIdTransaksi() {
-        return idTransaksi;
+    public Invoice(Transaksi transaksi, Pembayaran pembayaran) {
+        this.transaksi = transaksi;
+        this.pembayaran = pembayaran;
     }
-    
-    public void cetakInvoice() {
-        System.out.println("========== Invoice ==========");
-        System.out.println("ID Transaksi: " + idTransaksi);
-        System.out.println("Nama Customer: " + customer.getName());
-        System.out.println("Email Customer: " + customer.getName());
-        System.out.println("==================================");
-        System.out.println("Detail Barang:");
-        for (ItemKeranjang item : items) {
-            System.out.println("- " + item.getBarang().getNama() + 
-                               " (x" + item.getJumlah() + ") = " + 
-                               (item.getBarang().getHarga() * item.getJumlah()));
-        }
-        System.out.println("----------------------------------");
-        System.out.println("Total Harga: Rp " + totalHarga);
-        System.out.println("Metode Pembayaran: " + metodePembayaran);
-        System.out.println("==================================");
+}
+abstract class Pembayaran {
+    String id;
+
+    public Pembayaran(String id) {
+        this.id = id;
+    }
+}
+
+class QRIS extends Pembayaran {
+    public QRIS(String id) {
+        super(id);
+    }
+}
+
+class Bank extends Pembayaran {
+    public Bank(String id) {
+        super(id);
+    }
+}
+
+class COD extends Pembayaran {
+    public COD(String id) {
+        super(id);
     }
 }
