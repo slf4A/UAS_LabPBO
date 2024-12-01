5ecff67e7624cf8e0dca8c8a0599c8a6a859ba2a
SifaJ. <139639619+slf4A@users.noreply.github.com>
2024-12-01 09:19:30 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -1,17 +1,17 @@
 public class Akun {
   private String id;
 
-  // Constructor
+
   public Akun(String id) {
       this.id = id;
   }
 
-  // Getter for id
+ 
   public String getId() {
       return id;
   }
 
-  // Setter for id
+ 
   public void setId(String id) {
       this.id = id;
   }
